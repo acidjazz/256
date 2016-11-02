7580c010ef164da23ff3d91b76c67148470ae015
kevin olson <acidjazz@gmail.com>
2016-11-01 19:24:50 -0700

    fixed deploy script

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3cfg sync pub/. s3://256.io --exclude '.DS_Store'
+s3cmd --config=/Users/k/.s3cfg sync public/. s3://256.io --exclude '.DS_Store'
 s3cmd setacl --config=/Users/k/.s3cfg s3://256.io/ --acl-public --recursive
