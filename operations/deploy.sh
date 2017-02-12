de19cdc72459e6efeda31b3f09b81bdcdf24b4c0
kevin olson <acidjazz@gmail.com>
2017-02-11 18:41:57 -0800

    transition calming

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3cfg sync public/ s3://256.io --exclude '.DS_Store'
-s3cmd setacl --config=/Users/k/.s3cfg s3://256.io/ --acl-public --recursive
+s3cmd --config=/Users/k/.s3/.s3cfg sync public/ s3://256.io --exclude '.DS_Store'
+s3cmd setacl --config=/Users/k/.s3/.s3cfg s3://256.io/ --acl-public --recursive
